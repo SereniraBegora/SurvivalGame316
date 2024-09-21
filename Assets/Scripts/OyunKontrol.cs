e459e6f9919986cf888f3b33d1fbbffd559e2e25
SereniraBegora <bepalas16@gmail.com>
2024-09-21 18:19:29 +0300

    start project

---
+++ Assets/Scripts/OyunKontrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour {
+	public bool oyunAktif = true;
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void OyunaBasla(){
+		oyunAktif = true;
+	}
+}
